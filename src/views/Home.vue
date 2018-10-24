d43325b47b9b6ea5ae2c5c33f9d41058f034f961
Amanze Ogbonna <darlingtonamz@gmail.com>
2018-10-24 22:51:57 +0100

    Adding Vuetify to template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="home">
-    Home
-  </div>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Paletta
+        </h1>
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+    }
   }
-}
 </script>
